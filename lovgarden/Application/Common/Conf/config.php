a7fb36bb61f900c8b89109dae9094ad260d644ec
cary-cit <caryz@ciandt.com>
2018-04-13 12:56:22 +0800

    update config for aliyun

--- lovgarden/Application/Common/Conf/config.php
+++ lovgarden/Application/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     'DB_TYPE' =>  'mysql',
     'DB_DSN'    => 'mysql:host=127.0.0.1;dbname=lovgarden;charset=utf8',
     'DB_USER' =>  'root',
-    'DB_PWD' =>  'sherry418',
+    'DB_PWD' =>  'sherry418!',
     'DB_PORT' =>  '3306',
     'DB_PREFIX' =>  'lovgarden_',
     'DEFAULT_FILTER' => 'trim,htmlspecialchars',
@@ -24,8 +24,8 @@ return array(
     	'maxSize' => 1024*1024,
     	'exts' => array('jpg', 'gif', 'png', 'jpeg'), 
     	'rootPath' => './Public/Uploads/',  // 上传图片的保存路径  -> PHP要使用的路径，硬盘上的路径
-    	'viewPath' => '//local.lovgarden.com/Public/Uploads/',   // 显示图片时的路径    -> 浏览器用的路径，相对网站根目录
-        //'viewPath' => '//192.168.1.238/Public/Uploads/'
+    	//'viewPath' => '//local.lovgarden.com/Public/Uploads/',   // 显示图片时的路径    -> 浏览器用的路径，相对网站根目录
+        'viewPath' => '//47.98.216.142/Public/Uploads/'
     ),
     /************分页配置***************/
     'PRODUCT_VARIENT_PAGE' => array(
