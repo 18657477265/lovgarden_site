84d6f4bc5346454ef5a842fe496fee8f32ec371b
cary-cit <caryz@ciandt.com>
2018-04-15 21:13:04 +0800

    close debug mode

--- lovgarden/Application/Common/Conf/config.php
+++ lovgarden/Application/Common/Conf/config.php
@@ -18,7 +18,7 @@ return array(
     'MODULE_ALLOW_LIST' => array('Home','Admin'), 
     'APP_GROUP_LIST' => 'Home,Admin', //项目分组设定 
     'DEFAULT_GROUP' => 'Home', //默认分组
-    'DATA_CACHE_TYPE' => 'Memcache',
+    'DATA_CACHE_TYPE' => 'File',
     'MEMCACHE_HOST'  => 'tcp://127.0.0.1:11211',
     //'MEMCACHED_PORT' => array('11211'),
     'DATA_CACHE_TIME' => 7200,
@@ -27,8 +27,8 @@ return array(
     	'maxSize' => 1024*1024,
     	'exts' => array('jpg', 'gif', 'png', 'jpeg'), 
     	'rootPath' => './Public/Uploads/',  // 上传图片的保存路径  -> PHP要使用的路径，硬盘上的路径
-    	//'viewPath' => '//local.lovgarden.com/Public/Uploads/',   // 显示图片时的路径    -> 浏览器用的路径，相对网站根目录
-        'viewPath' => '//47.98.216.142/Public/Uploads/'
+    	'viewPath' => '//www.flowerideas.cn/Public/Uploads/',   // 显示图片时的路径    -> 浏览器用的路径，相对网站根目录
+        //'viewPath' => '//47.98.216.142/Public/Uploads/'
     ),
     /************分页配置***************/
     'PRODUCT_VARIENT_PAGE' => array(
