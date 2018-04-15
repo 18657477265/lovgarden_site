d46138de722efb799c6ab07af88ea7fb56830a7c
cary-cit <caryz@ciandt.com>
2018-04-15 22:28:04 +0800

    update config for cached page

--- lovgarden/Application/Home/Conf/config.php
+++ lovgarden/Application/Home/Conf/config.php
@@ -2,9 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'HTML_CACHE_ON'     =>    true, // 开启静态缓存
-	'HTML_CACHE_TIME'   =>    60,   // 全局静态缓存有效期（秒）
+	'HTML_CACHE_TIME'   =>    3600,   // 全局静态缓存有效期（秒）
 	'HTML_FILE_SUFFIX'  =>    '.html', // 设置静态缓存文件后缀
 	'HTML_CACHE_RULES'  =>     array(  // 定义静态缓存规则
-	   'index:index' => array('index' => 86400),
+	   'Index:index' => array('{:controller}_{:action}'),
+	   'Product:select_list' => array('{:controller}_{:action}'),
     )
 );
