97a6996456f4bf114a800aab0fd60aeee925e6ed
cary-cit <caryz@ciandt.com>
2018-06-04 11:37:38 +0800

    fix return url

--- lovgarden/Application/Common/Common/function.php
+++ lovgarden/Application/Common/Common/function.php
@@ -413,7 +413,7 @@ function translate_status_label($status_code = '2') {
         case '6':
             $result = '您的订单提交成功';
             break;
-        case '6':
+        case '7':
             $result = '订单付款成功';
             break;
     }
