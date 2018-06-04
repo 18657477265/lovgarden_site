1be4e76aa64c5c71c0f59b7eeeda6c1d1b1f4dc3
cary-cit <caryz@ciandt.com>
2018-06-04 11:29:55 +0800

    debug pay logic

--- lovgarden/Application/Home/Controller/UserController.class.php
+++ lovgarden/Application/Home/Controller/UserController.class.php
@@ -298,16 +298,16 @@ class UserController extends Controller {
     }
 
     public function user_order_handle() {
-       $log_file = '/a.txt';
-       $test = serialize($_POST);
-       file_put_contents($log_file,$test, FILE_APPEND);
-       exit();
+       //$log_file = '/a.txt';
+       //$test = serialize($_POST);
+       //file_put_contents($log_file,$test, FILE_APPEND);
+       //exit();
 
-       echo "<pre>";
-       echo '2';
-       print_r($_POST);
-       echo "</pre>";
-       exit();
+       //echo "<pre>";
+       //echo '2';
+       //print_r($_POST);
+       //echo "</pre>";
+       //exit();
        $order = D('Order');
        $order->order_handle_notify();
     }
