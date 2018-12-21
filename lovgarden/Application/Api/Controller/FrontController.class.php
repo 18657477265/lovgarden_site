f7540cc231ca8f1439bf9b340f0806a1fbaaac9b
18657477265 <18657477265@163.com>
2018-12-21 13:45:18 +0800

    add front page api

---
+++ lovgarden/Application/Api/Controller/FrontController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Api\Controller;
+use Think\Controller\RestController;
+use Think\Cache\Driver\Memcache;
+class FrontController extends RestController {
+   public function index() {
+       $block_model = D('Admin/Block');
+       $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'front' ORDER BY block_order ASC;";
+       $blocks = $block_model->query($sql);
+       echo json_encode(array(
+            'block' => $blocks
+       ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+   }  
+}
