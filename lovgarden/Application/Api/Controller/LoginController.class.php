a6091a65b56770164eb138f7a3c90f7814ab66a3
18657477265 <18657477265@163.com>
2018-12-27 20:21:23 +0800

    add wxuser api

--- lovgarden/Application/Api/Controller/LoginController.class.php
+++ lovgarden/Application/Api/Controller/LoginController.class.php
@@ -16,15 +16,17 @@ class LoginController extends RestController {
        //根据openID创建一个微信用户
        //将session_key保存到缓存中,以便小程序检查登录状态
        $login_code = 'Login Error';
+       $mem_cache = new Memcache();
        if(!empty($session_key) && !empty($openid)) {
-           $mem_cache = new Memcache();
            $mem_cache->set($login_code, $session_key, 3600);
            $login_code = md5($openid.$session_key);
            $wx_user = D('Wxuser');      
            $wx_user->add_wxuser($openid);
        }
        echo json_encode(array(
-            'loginInfo' => $login_code
+            'loginInfo' => $login_code,
+            'session' => $session_key,
+            'cache'=>  $mem_cache->get($login_code)
        ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
        //return $this->result(0, 'success', $session_key);//返回给前台一个sess
    }
