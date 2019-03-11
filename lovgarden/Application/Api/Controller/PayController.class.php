a64051987d851a00df143b07f58d6da7dc8796a6
18657477265 <18657477265@163.com>
2019-03-11 14:14:59 +0800

    fix create order module

--- lovgarden/Application/Api/Controller/PayController.class.php
+++ lovgarden/Application/Api/Controller/PayController.class.php
@@ -4,13 +4,25 @@ use Think\Controller\RestController;
 use Think\Cache\Driver\Memcache;
 class PayController extends RestController {
    public function create_wx_order() {
+               
+         //echo "<pre>";
+         //print_r($_GET['sku_ids']);
+         //echo "</pre>";
+         //exit();
+         
          $login_ip = I('get.login_ip');
-         $sku_ids = I('get.sku_ids');
+         //$sku_ids = I('get.sku_ids');
+         $sku_ids = json_decode($_GET['sku_ids']);
+         //$sku_ids = json_decode($sku_ids);
+         //echo "<pre>";
+         //print_r($sku_ids[0]->deliver_time);
+         //echo "</pre>";
+         //exit();
          $vase_count = I('get.vase_count');
          $coupon_code = I('get.coupon_code');
          $mem_cache = new Memcache();
-         //$login_exist = $mem_cache->get($login_ip);
-         $login_exist = 1;
+         $login_exist = $mem_cache->get($login_ip);
+         /*$login_exist = 1;
          $sku_ids = array(
            0 => array(
                'sku_id'=>'180011',
@@ -24,13 +36,13 @@ class PayController extends RestController {
                'vase_option' => '1',
                'deliver_time' => '2019-05-09 23:59:58'
            )
-         );
+         );*/
          $sku_ids_str = '';
          $count = 0;
          $sku_ids_count = count($sku_ids);
          foreach ($sku_ids as $k => $v) {
              $count = $count + 1;
-             $sku_ids_str = $sku_ids_str . $v['sku_id'];
+             $sku_ids_str = $sku_ids_str . $v->sku_id;
              if($count != $sku_ids_count){
                 $sku_ids_str = $sku_ids_str . ',';         
              }
@@ -43,13 +55,13 @@ class PayController extends RestController {
              //$multiple_fileds_array = array('image_url','flower_home_id','hurry_level_id','flower_home');  
              $result_rows_array = translate_database_result_to_logic_array($result_rows,array(),'sku_id');
              foreach ($sku_ids as $k => $v) {
-                 $result_rows_array[$v['sku_id']]['count']=$v['count'];
-                 $result_rows_array[$v['sku_id']]['vase']=$v['vase_option'];
-                 $result_rows_array[$v['sku_id']]['deliver_time']=$v['deliver_time'];
+                 $result_rows_array[$v->sku_id]['count']=$v->count;
+                 $result_rows_array[$v->sku_id]['vase']=$v->vase_option;
+                 $result_rows_array[$v->sku_id]['deliver_time']=$v->deliver_time;
              }
              $error_message = '';
              $order_products_info = $result_rows_array;
-             $costs = wx_calculate_cost($order_products_info,20,$coupon_code,$vase_count);                    
+             $costs = wx_calculate_cost($order_products_info,20,$coupon_code,$vase_count);                   
              $order_info = array(
                 'order_id' => date('ymdHis'). rand(10000,99999),
                 'order_owner' => $login_exist,
@@ -69,12 +81,17 @@ class PayController extends RestController {
                 'order_vip_level_cut' => '0',
                 'order_final_price' => $costs['total_cost'],
                 'order_status' => '1',  
-             );         
+             );
+        
              $order_model = D('Order');
              $validate_status = $order_model->create($order_info);
+             //print_r($validate_status);
+             //exit();
              if($validate_status){
                  $order_model->startTrans();
                  $order_id = $order_model->add($validate_status);
+                 //echo $order_id;
+                 //exit();
                  if($order_id) {
                      $final_status = TRUE;
                      $order_bind_product_model = D('OrderProductVarient');
@@ -97,7 +114,7 @@ class PayController extends RestController {
                     if($final_status) {
                        $order_model->commit();
                        echo json_encode(array(
-                          'create_order' => TRUE
+                          'create_order' => '200'
                        ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
                        exit();
                     }
@@ -105,7 +122,7 @@ class PayController extends RestController {
              }
          }
     echo json_encode(array(
-         'create_order' => FALSE
+         'create_order' => '404'
     ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
     exit();        
    }
