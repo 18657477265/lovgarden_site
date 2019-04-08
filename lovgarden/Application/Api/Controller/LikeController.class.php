26caeb82282c26487904eb59e9982447597db910
18657477265 <18657477265@163.com>
2019-04-08 10:24:04 +0800

    test like module

--- lovgarden/Application/Api/Controller/LikeController.class.php
+++ lovgarden/Application/Api/Controller/LikeController.class.php
@@ -8,6 +8,14 @@ use SimpleXMLElement;
 class LikeController extends RestController {
    public function addXmlToMemory() {
        //启动任务和每周定时任务,将xml文件中的数据读入到内存中去
+       $xmlTag = array('id','sku_id','varient_name','likes');
+       $products = array();
+       $xml = simplexml_load_file('/xmls/products_likes.xml');
+       foreach($xml->children() as $product) {
+         $products[] = get_object_vars($product);//获取对象全部属性，返回数组
+       }
+       echo '<pre>';
+       print_r($products);
    }
    public function addLikes() {
        //操作内存中的数据
@@ -25,7 +33,10 @@ class LikeController extends RestController {
        $model = new \Think\Model();
        $data = $model->query($sql);
        $products = translate_database_result_to_logic_array($data,array(),'id');
-       
+       echo "<pre>";
+       print_r($products);
+       echo "</pre>";
+       exit();
        $xmlTag = array('id','sku_id','varient_name','likes');
        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><products />');
        foreach($products as $item) {
