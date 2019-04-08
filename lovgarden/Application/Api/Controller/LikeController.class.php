67771ba05e9cd888aa6619646675eff62322d2c4
18657477265 <18657477265@163.com>
2019-04-08 12:35:39 +0800

    add likes module

--- lovgarden/Application/Api/Controller/LikeController.class.php
+++ lovgarden/Application/Api/Controller/LikeController.class.php
@@ -8,15 +8,19 @@ use SimpleXMLElement;
 class LikeController extends RestController {
    public function addXmlToMemory() {
        //启动任务和每周定时任务,将xml文件中的数据读入到内存中去
-       $xmlTag = array('id','sku_id','varient_name','likes');
-       $products = array();
-       $xml = simplexml_load_file('/xmls/products_likes.xml');
-       foreach($xml->children() as $product) {
-         $item = get_object_vars($product);//获取对象全部属性，返回数组
-         $products[$item['id']] = $item;
+       $ip = getIP();
+       if($ip == '127.0.0.1' || $ip == '47.98.216.142' || $ip == '172.16.207.38') {
+         $xmlTag = array('id','sku_id','varient_name','likes');
+         $products = array();
+         $xml = simplexml_load_file('/xmls/products_likes.xml');
+         foreach($xml->children() as $product) {
+           $item = get_object_vars($product);//获取对象全部属性，返回数组
+           $products[$item['id']] = $item;
+         }
+         $mem_cache = new Memcache();
+         $mem_cache->set('likes_products',$products,604800);
+         file_put_contents('/xmls/cron_products_likes.log',date('Y-m-d H:i:s',time())." Add products_likes.xml To Memory".PHP_EOL,FILE_APPEND);
        }
-       $mem_cache = new Memcache();
-       $mem_cache->set('likes_products',$products,604800);
    }
    public function addLikes($id) {
        //操作内存中的数据
@@ -36,17 +40,21 @@ class LikeController extends RestController {
    }
    public function addMemoryToXml(){
        //每天定时任务,将最新的内存中的数组写入到xml文件中去
-       $mem_cache = new Memcache();
-       $products = $mem_cache->get('likes_products');
-       $xmlTag = array('id','sku_id','varient_name','likes');
-       $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><products />');
-       foreach($products as $item) {
-          $product = $xml->addChild('product');
-          foreach($xmlTag as $x) {
-              $product->addChild($x, $item[$x]);
-          }
+       $ip = getIP();
+       if($ip == '127.0.0.1' || $ip == '47.98.216.142' || $ip == '172.16.207.38') {
+         $mem_cache = new Memcache();
+         $products = $mem_cache->get('likes_products');
+         $xmlTag = array('id','sku_id','varient_name','likes');
+         $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><products />');
+         foreach($products as $item) {
+            $product = $xml->addChild('product');
+            foreach($xmlTag as $x) {
+                $product->addChild($x, $item[$x]);
+            }
+         }
+         $xml->asXml('/xmls/products_likes.xml');
+         file_put_contents('/xmls/cron_products_likes.log',date('Y-m-d H:i:s',time())." Add Product_likes Memory To products_likes.xml".PHP_EOL,FILE_APPEND);
        }
-       $xml->asXml('/xmls/products_likes.xml');
    }
    public function makeXml() {
        //将数据库里面的数据制作成本地XML文件,数据初始化
