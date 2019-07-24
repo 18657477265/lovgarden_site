b59c9ce967f4795c5220b317c5f76e090ff1b906
18657477265 <18657477265@163.com>
2019-07-24 21:35:49 +0800

    Add recharge module

---
+++ lovgarden/Application/Api/Controller/RechargeController.class.php
@@ -0,0 +1,125 @@
+<?php
+namespace Api\Controller;
+use Think\Controller\RestController;
+use Think\Cache\Driver\Memcache;
+use Api\Model\WxpayModel;
+class RechargeController extends RestController {      
+   //创建充值订单
+   public function makeRecharge($original_pay,$login_ip='none') {
+       $mem_cache = new Memcache();
+       $login_exist = $mem_cache->get($login_ip);
+       $add_result = 0;
+       $data = array();
+       $create_recharge = 404;
+       if(!empty($login_exist)) {              
+         //获取阈值,避免数据库被恶意写入
+         $login_ip_existed_try = $mem_cache->get($login_exist."recharge");
+         if(empty($login_ip_existed_try)) {
+             $login_ip_existed_try = 0;
+         }
+         $login_ip_existed_try =  $login_ip_existed_try + 1;
+         //半小时以内最多只能创建五个空的充值订单
+         $mem_cache->set($login_exist."recharge",$login_ip_existed_try,1800);
+         if($login_ip_existed_try < 5) {
+             $open_id = $login_exist;
+             $recharge = D('Recharge');
+             $gift_pay = $recharge->findGiftPayCost($original_pay);
+             $recharge_info = array(
+                'recharge_id' => date('ymdHis'). rand(10000,99999),
+                'open_id' => $open_id,
+                'original_pay' => $original_pay,
+                'gift_pay' => $gift_pay,
+                'result_pay' => $original_pay + $gift_pay,
+                'recharge_create_time' => date('Y-m-d H:i:s')
+             );
+            $add_result = $recharge->addRecharge($recharge_info);
+            if($add_result > 0) {
+                $create_recharge = 500;
+                //创建微信支付预订单
+                $wx_pay_model = new WxpayModel('wxd7561da4052911c3', '1526072861', 'https://www.flowerideas.cn/api/recharge/userRechargeSuccess', '9xtnukxfqwvid4it94ieu736lktnc3mu',$login_exist);
+                $params['body'] = '花点馨思花卉充值';
+                $params['out_trade_no'] = $recharge_info['recharge_id'];
+                $params['total_fee'] = $recharge_info['original_pay'];
+                $params['trade_type'] = 'JSAPI';
+                $result = $wx_pay_model->unifiedOrder( $params );
+                if(!empty($result['prepay_id'])) {
+                   $data = $wx_pay_model->getPayParams($result['prepay_id']);
+                   $create_recharge = 200;
+                }
+            }
+         }
+        }
+       echo json_encode(array(
+          'data' => $data,
+          'create_recharge' => $create_recharge
+       ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+   }
+   public function userRechargeSuccess() {
+       //接受微信支付通知所传递的微信充值单号和金额
+       $xml = file_get_contents("php://input");
+       if( empty($xml) ){
+           //file_put_contents('/b.txt', $xml, FILE_APPEND);
+           return false;
+       }
+       $result = translate_xml_to_data( $xml );
+       $get_sign = $result['sign'];
+       unset($result['sign']);
+       $string = '';
+       if( !empty($result) ){
+           $array = array();
+           foreach( $result as $key => $value ){
+             $array[] = $key.'='.$value;
+           }
+           $string = implode("&",$array);
+       }
+       $string = $string . "&key=".'9xtnukxfqwvid4it94ieu736lktnc3mu';
+       $string = md5($string);
+       $check_sign = strtoupper($string);
+       if($get_sign != $check_sign) {
+           return false;
+           exit();
+       }
+       $out_trade_no = $result['out_trade_no'];
+       $total_amount = $result['total_fee'];
+       $trade_no = $result['transaction_id'];
+       if(!empty($out_trade_no)){
+         //$yuan = $total_amount/100;
+          $yuan = $total_amount;     
+         //$recharge_id = '19072021551048956';
+         //$original_pay = 1200;
+         $recharge = D('Recharge');
+         $gift_pay = $recharge->findGiftPayCost($yuan);
+         $result_pay = $yuan + $gift_pay;
+         //事务:充值表状态改为充值成功,并给用户添加上响应的充值金额
+         $sql_pay = "UPDATE lovgarden_recharge SET `status`= '2' WHERE recharge_id = '$out_trade_no' AND `status`= '1'";
+         $sql_add_recharge_pay = "UPDATE lovgarden_wxuser AS a SET a.balance = a.balance + $result_pay WHERE a.open_id = (SELECT b.open_id FROM lovgarden_recharge as b WHERE b.recharge_id ='$out_trade_no')";
+          //file_put_contents('/b.txt', '------'.$sql_pay, FILE_APPEND);
+          //如果有优惠券,还需要吧优惠券设置成已使用
+         $recharge->startTrans();
+         $sql_result = $recharge->execute($sql_pay);
+         if($sql_result) {
+             $add_user_pay_result = $recharge->execute($sql_add_recharge_pay);
+             if($add_user_pay_result) {
+                 $recharge->commit();
+               //提交微信停止通知
+                 echo "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
+                 exit();
+             }
+             else {
+                 $recharge->rollback();
+                 exit();
+             }
+         }
+         else {
+            $recharge->rollback();
+            exit();
+         }
+       }
+   }
+   public function userPayWithBalance($order_id) {
+       $recharge = D("Recharge");
+       $pay_result = $recharge->userPayWithBalance($order_id);
+       echo $pay_result;
+   }
+   
+}
---
+++ lovgarden/Application/Api/Model/RechargeModel.class.php
@@ -0,0 +1,93 @@
+<?php
+namespace Api\Model;
+use Think\Model;
+use Think\Cache\Driver\Memcache;
+class RechargeModel extends Model 
+{
+   public function addRecharge($recharge_array) {
+        $result = $this->add($recharge_array);
+        return $result;
+    }
+   public function findGiftPayCost($original_pay) {
+       $activitys_rows = $this->getActivityData("gift_pay","1");
+       $level = $this->findRightLevelActivity($activitys_rows, $original_pay);
+       if($level != '-1') {
+           return $activitys_rows[$level]['gift_pay']; 
+       }
+       else {
+          return 0;
+       }
+   }
+   function getActivityData($type,$status) {
+       $activitys = new Memcache();
+       $activitys_rows = $activitys->get('activitys_rows');
+       if(empty($activitys_rows)){
+         $sql = "SELECT * FROM lovgarden_activity WHERE `type` = '$type' AND `status` = '$status' ORDER BY if_pay ASC";
+         $activitys_rows = $this->query($sql);
+         $activitys->set('activitys_rows', $activitys_rows,86400);
+       }
+       return $activitys_rows;
+   }
+   function findRightLevelActivity($activitys_rows,$original_pay) {
+       if(!empty($activitys_rows)) {
+         foreach($activitys_rows as $key => $value) {
+             if($value["if_pay"]<$original_pay) {           
+               if(!empty($activitys_rows[$key+1])){
+                   continue;
+               }
+               else {
+                   return $key;
+               }
+             }
+             else {
+                 $index = $key - 1;
+                 return $index;
+             }
+         }
+       }
+       else {
+           return -1;
+       }
+   }
+   public function userPayWithBalance($order_id) {
+       $sql = "select order_owner, order_final_price from lovgarden_order where order_id = '$order_id'";
+       $order_info = $this->query($sql);
+       $order_final_price = $order_info[0]["order_final_price"];
+       $open_id = $order_info[0]["order_owner"];
+       $balance = $this->getUserBalance($open_id);
+       if($balance > $order_final_price) {
+           //将用户余额减去订单金额
+           $sql_pay_with_balance = "UPDATE lovgarden_wxuser AS a SET a.balance=a.balance - '$order_final_price',a.reward_points = a.reward_points + '$order_final_price' WHERE open_id = '$open_id'";
+           $sql_update_order = "UPDATE lovgarden_order SET order_status = '2' WHERE order_id = '$order_id' AND order_status = '1'";
+           $this->startTrans();
+           $update_balance_result = $this->execute($sql_pay_with_balance);
+           if($update_balance_result) {
+               $update_order_result = $this->execute($sql_update_order);
+               if($update_order_result) {
+                   $this->commit();
+                   return 'success';
+               }
+               else {
+                   $this->rollback();
+                   return 'update order error';
+               }
+           }
+           else {
+             $this->rollback();
+             return "pay with balance error";
+           }
+       }
+       else {
+           return 'balance not enough';
+       }
+   }
+   function getUserBalance($open_id) {
+       $sql = "select balance from lovgarden_wxuser where open_id = '$open_id'";
+       $current_balance = $this->query($sql);
+       $balance = $current_balance[0]["balance"];
+       return $balance;
+   }
+}
+
+
+
