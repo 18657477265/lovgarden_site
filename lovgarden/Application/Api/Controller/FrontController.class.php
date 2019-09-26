485046cde8c599a1887dd4018697c2ea4d2d41c1
18657477265 <18657477265@163.com>
2019-09-26 14:01:06 +0800

    update front page block

--- lovgarden/Application/Api/Controller/FrontController.class.php
+++ lovgarden/Application/Api/Controller/FrontController.class.php
@@ -5,7 +5,7 @@ use Think\Cache\Driver\Memcache;
 class FrontController extends RestController {
    public function index() {
        $block_model = D('Admin/Block');
-       $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'front' ORDER BY block_order ASC;";
+       $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatFront' ORDER BY block_order ASC;";
        $blocks = $block_model->query($sql);
        echo json_encode(array(
             'block' => $blocks
