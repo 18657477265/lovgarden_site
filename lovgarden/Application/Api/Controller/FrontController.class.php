c8ca9fb29ee1494a06fefb6d86f6cba1970afcb5
18657477265 <18657477265@163.com>
2019-10-01 19:23:42 +0800

    add cache for front page

--- lovgarden/Application/Api/Controller/FrontController.class.php
+++ lovgarden/Application/Api/Controller/FrontController.class.php
@@ -4,15 +4,23 @@ use Think\Controller\RestController;
 use Think\Cache\Driver\Memcache;
 class FrontController extends RestController {
    public function index() {
-       $block_model = D('Admin/Block');
-       $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatFront' ORDER BY block_order ASC;";
-       $blocks = $block_model->query($sql);
+       $mem_cache = new Memcache();
+       //$front_blocks_array = array();
+       $front_blocks_array = $mem_cache->get('front_blocks');
+       if(empty($front_blocks_array)) {
+         $block_model = D('Admin/Block');
+         $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatFront' ORDER BY block_order ASC;";
+         $blocks_tip = $block_model->query($sql);
        
-       $sql_front_activity = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatActivity' ORDER BY block_order ASC;";
-       $blocks_activity = $block_model->query($sql_front_activity);
+         $sql_front_activity = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatActivity' ORDER BY block_order ASC;";
+         $blocks_activity = $block_model->query($sql_front_activity);
+         $front_blocks_array['block_tip'] = $blocks_tip;
+         $front_blocks_array['block_activity'] = $blocks_activity;
+         $mem_cache->set('front_blocks',$front_blocks_array,86400);
+       }
        echo json_encode(array(
-            'block' => $blocks,
-            'activity_block' => $blocks_activity
+            'block' => $front_blocks_array['block_tip'],
+            'activity_block' => $front_blocks_array['block_activity']
        ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
    }  
 }
