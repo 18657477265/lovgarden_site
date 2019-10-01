8f7e96aa43f2e258a6c4c305c67f299d330341e3
18657477265 <18657477265@163.com>
2019-10-01 15:39:21 +0800

    add activity blocks for front page

--- lovgarden/Application/Api/Controller/FrontController.class.php
+++ lovgarden/Application/Api/Controller/FrontController.class.php
@@ -7,8 +7,12 @@ class FrontController extends RestController {
        $block_model = D('Admin/Block');
        $sql = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatFront' ORDER BY block_order ASC;";
        $blocks = $block_model->query($sql);
+       
+       $sql_front_activity = "SELECT image_pc,image_mobile,block_title,block_body,block_link_title,block_link,page_link FROM lovgarden_block WHERE page_link = 'WeChatActivity' ORDER BY block_order ASC;";
+       $blocks_activity = $block_model->query($sql_front_activity);
        echo json_encode(array(
-            'block' => $blocks
+            'block' => $blocks,
+            'activity_block' => $blocks_activity
        ),JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
    }  
 }
